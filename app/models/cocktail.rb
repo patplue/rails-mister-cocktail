453dc52c71358e1c5aaf40a3cb48dc43b67eb8c3
Patrick Plue <pluepatrick@gmail.com>
2020-02-20 14:37:39 +0100

    added the relations in the models

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,5 @@
 class Cocktail < ApplicationRecord
+  has_many :ingredients, through: :doses
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,5 @@
 class Ingredient < ApplicationRecord
+  has_many :doses, through: :ingredients
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
