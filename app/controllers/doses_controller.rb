bc5a1a29b5c46e93d519e277f193d38ee2a7ef04
Patrick Plue <pluepatrick@gmail.com>
2020-02-21 16:26:45 +0100

    added the links between pages and the delete funtions for doses and cocktails

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,4 +3,28 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.delete
+    redirect_to @cocktail
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails do
-    resources :doses, only: [:new, :create]
+  resources :cocktails, except: :index do
+    resources :doses, only: [:new, :create, :destroy]
   end
 end
